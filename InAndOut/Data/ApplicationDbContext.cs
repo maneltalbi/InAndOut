27a3c521f39b79dfe64757fa291f6c5e7d3377e2
manel talbi <talbimanel20@gmail.com>
2022-01-26 15:28:00 +0100

    Configure services

--- InAndOut/Data/ApplicationDbContext.cs
+++ InAndOut/Data/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InAndOut.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext :DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+      public DbSet<Item> Items { get; set; }
     }
 }

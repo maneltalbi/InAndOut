99abe7c26f8259d26997dfe88de7ecb8e3ca00be
manel talbi <talbimanel20@gmail.com>
2022-01-28 12:54:05 +0100

    Delete expenses

--- InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/Controllers/ExpenseController.cs
@@ -41,5 +41,40 @@ namespace InAndOut.Controllers
            
 
         }
+
+        //Get Delelte
+       
+        public IActionResult Delete(int? id)
+        {
+           
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+
+        }
+        //post Delelte
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+           
+                _db.Expenses.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+    
+        }
     }
 }
